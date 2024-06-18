7af7e82c264720435bc958f1f23f605e10246664
NelsonH <nelson.hoang@gmail.com>
2024-06-18 13:59:12 -0700

    [PhotoList] Add AsyncImage to display images of Mars

--- app/src/main/java/com/nhoang/marsrover/ui/view/PhotoList.kt
+++ app/src/main/java/com/nhoang/marsrover/ui/view/PhotoList.kt
@@ -2,6 +2,7 @@ package com.nhoang.marsrover.ui.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.nhoang.marsrover.R
 import com.nhoang.marsrover.domain.model.RoverPhotoUiModel
 
@@ -42,6 +44,12 @@ fun Photo(
             Text(
                 text = roverPhotoUiModel.roverName,
                 modifier = Modifier.padding(16.dp)
+            )
+
+            AsyncImage(
+                model = roverPhotoUiModel.imgSrc,
+                contentDescription = "rover photo",
+                modifier = Modifier.height(300.dp)
             )
 
             Text(text = stringResource(id = R.string.sol, roverPhotoUiModel.sol))
