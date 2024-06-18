09c06c75ca4e986b81fb9f4dc6d309f901ded79b
NelsonH <nelson.hoang@gmail.com>
2024-06-18 13:48:20 -0700

    [PhotoScreen] Implement PhotoList UI

--- app/src/main/java/com/nhoang/marsrover/ui/photolist/MarsRoverPhotoViewModel.kt
+++ app/src/main/java/com/nhoang/marsrover/ui/photolist/MarsRoverPhotoViewModel.kt
@@ -3,19 +3,30 @@ package com.nhoang.marsrover.ui.photolist
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nhoang.marsrover.data.MarsRoverPhotoRepo
+import com.nhoang.marsrover.domain.model.RoverPhotoUiModel
+import com.nhoang.marsrover.domain.model.RoverPhotoUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MarsRoverPhotoViewModel @Inject constructor(
     private val marsRoverPhotoRepo: MarsRoverPhotoRepo
-): ViewModel() {
+) : ViewModel() {
+
+    private val _roverPhotoUiState: MutableStateFlow<RoverPhotoUiState> =
+        MutableStateFlow(RoverPhotoUiState.Loading)
+
+    val roverPhotoUiState: StateFlow<RoverPhotoUiState>
+        get() = _roverPhotoUiState
 
     fun getMarsRoverPhoto(roverName: String, sol: String) {
         viewModelScope.launch {
-            marsRoverPhotoRepo.getMarsRoverPhoto(roverName, sol).collect{
-
+            _roverPhotoUiState.value = RoverPhotoUiState.Loading
+            marsRoverPhotoRepo.getMarsRoverPhoto(roverName, sol).collect {
+                _roverPhotoUiState.value = it
             }
         }
     }
--- app/src/main/java/com/nhoang/marsrover/ui/view/PhotoScreen.kt
+++ app/src/main/java/com/nhoang/marsrover/ui/view/PhotoScreen.kt
@@ -3,6 +3,9 @@ package com.nhoang.marsrover.ui.view
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.nhoang.marsrover.domain.model.RoverPhotoUiState
 import com.nhoang.marsrover.ui.photolist.MarsRoverPhotoViewModel
 
 @Composable
@@ -11,10 +14,16 @@ fun PhotoScreen(
     sol: String?,
     marsRoverPhotoViewModel: MarsRoverPhotoViewModel
 ) {
+    val viewState by marsRoverPhotoViewModel.roverPhotoUiState.collectAsStateWithLifecycle()
+
     if (roverName != null && sol != null) {
         LaunchedEffect(Unit) {
             marsRoverPhotoViewModel.getMarsRoverPhoto(roverName, sol)
         }
+        when (val roverPhotoUiState = viewState) {
+            RoverPhotoUiState.Error -> Error()
+            RoverPhotoUiState.Loading -> Loading()
+            is RoverPhotoUiState.Success -> PhotoList(roverPhotoUiModelList = roverPhotoUiState.roverPhotoUiModelList)
+        }
     }
-    Text(text = "Photo screen")
 }
