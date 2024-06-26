b46d197b6cbc533bf8ab4f072e3af25995e1d152
NelsonH <nelson.hoang@gmail.com>
2024-06-25 22:24:32 -0700

    [PhotoList] Add save button

--- app/src/main/java/com/nhoang/marsrover/ui/view/PhotoList.kt
+++ app/src/main/java/com/nhoang/marsrover/ui/view/PhotoList.kt
@@ -1,7 +1,10 @@
 package com.nhoang.marsrover.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,7 +24,7 @@ import com.nhoang.marsrover.domain.model.RoverPhotoUiModel
 
 @Composable
 fun PhotoList(
-    modifier : Modifier,
+    modifier: Modifier,
     roverPhotoUiModelList: List<RoverPhotoUiModel>
 ) {
     Surface(color = MaterialTheme.colorScheme.background, modifier = modifier) {
@@ -42,6 +46,23 @@ fun Photo(
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = painterResource(
+                        id = if (roverPhotoUiModel.isSaved) {
+                            R.drawable.ic_save
+                        } else {
+                            R.drawable.ic_save_outline
+                        }
+                    ),
+                    contentDescription = "Save icon"
+
+                )
+                Text(
+                    text = roverPhotoUiModel.roverName,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
             Text(
                 text = roverPhotoUiModel.roverName,
                 modifier = Modifier.padding(16.dp)
